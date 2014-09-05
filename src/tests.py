41ae3e890312e89dc50f70277b60686964e97f47
Matthew McGovern <mmcgover@oswego.edu>
2014-09-05 15:00:17 -0400

    updated regex, logic \ncreated better tests for identifying different specific double type numbers in text, lat/lons, scientific notation doubles, and then just regular doubles.

--- src/tests.py
+++ src/tests.py
@@ -1,6 +1,7 @@
 import unittest
 import tools
 from tools import *
+
 class TestTools(unittest.TestCase):
 
 	def setUp(self):
@@ -9,17 +10,18 @@ class TestTools(unittest.TestCase):
 		self.line = "Here's a line of test text" + str(self.double1) + ":$:" +str(self.double2)
 
 
-	def test_find_double1(self):
-		self.assertEquals(tools.find_double(str(self.double1)), self.double1  )
+	def test_find_latlon1(self):
+		self.assertEquals(tools.find_latlon(str(self.double1)), self.double1  )
 
-	def test_find_double2(self):
-		self.assertEquals(tools.find_double(str(self.double2)), self.double2  )
+	def test_find_latlon2(self):
+		self.assertEquals(tools.find_latlon(str(self.double2)), self.double2  )
 
-	def test_find_double3(self):
+	def test_find_latlon3(self):
 		thistest= "-9000.0000"
 		try:
-			thedouble = tools.find_double(thistest)
-		except NoDoubleError as error:
+			thedouble = tools.find_latlon(thistest)
+			self.fail("No error was thrown while parsing an invalic double")
+		except NoLatLonError as error:
 			#print "We didn't parse the double" ##Good, this worked!
 			pass 
 			''' LOGICNOTE: This should happen, so unless we hit the
@@ -29,6 +31,26 @@ class TestTools(unittest.TestCase):
 			#If we get here, an exception was not raised!
 			## Fail! fail like a sophomore!
 			self.fail("The following value should not have parsed as a double: " + thedouble )
+
+	
+	def test_find_latlon4(self):
+		thistest= "2040"
+		try:
+			thedouble = tools.find_latlon(thistest)
+			self.fail("No error was thrown while parsing an invalic number " + thistest)
+		except NoLatLonError as error:
+			#print "We didn't parse the double" ##Good, this worked!
+			pass 
+			''' LOGICNOTE: This should happen, so unless we hit the
+			 else,it should just do nothing and this
+			 will count as a win.'''
+		else:
+			#If we get here, an exception was not raised!
+			## Fail! fail like a sophomore!
+			self.fail("The following value should not have parsed as a double: " + thedouble )
+	def test_find_latlon_in_text(self):
+		self.assertEquals(tools.find_latlon(self.line), self.double1  )
+		
 
 
 if __name__ =='__main__':
--- src/tools.py
+++ src/tools.py
@@ -7,13 +7,37 @@ import re
 @github github.com/mcgov
 """
 
-ONE_DOUBLE = re.compile(r"\-?(?<![0-9])[0-9]{1,3}\.[0-9]{1,15}")
+ONE_LATLON = re.compile(r"\-?(?<![0-9])[0-9]{1,3}\.[0-9]{1,15}")
 
-class NoDoubleError(Exception):
+## Finds a lat/lon style point, between one and three leading digits,
+##Up to 15 trailing digits. 
+## TODO: This could be better if it searched more specifically...
+	## Maybe only searched for values within the proper ranges. -180,180 90,90
+
+ONE_DOUBLE = re.compile(r"\-?(?<![0-9])[0-9]{1,15}\.[0-9]{1,15}")
+## Searches for a double, between 1-15 leading digits, 1-15 trailing digits.
+ALT_DOUBLE = re.compile(r"\-?(?<![0-9])[0-9]\.?[0-9]{0,10}[Ee][0-9]{1,10}"))
+## Searches for alternate scientific notation doubles, 1.23e200 or 1E20 or 1.2234E12300
+## Must have 1 leading digit, may or may not have decimal digits in the significand,
+## must have either e or E, and at least one expontential digit.
+
+class NoLatLonError(Exception):
 	def __init__(self,line):
 		self.the_bad_line = line
 	def __str__(self):
 		return repr(self.the_bad_line)
+
+def find_latlon(line):
+	"""In a line of text, find the first double in that line of
+	 text and return it (as a double).
+	"""
+	matches = ONE_LATLON.search(line)
+	if matches:
+		return float( matches.group(0) )
+
+	else:
+		raise NoLatLonError( line )
+		"""Return the group if it has found a match. If not: raise our error."""
 
 def find_double(line):
 	"""In a line of text, find the first double in that line of
@@ -24,7 +48,6 @@ def find_double(line):
 		return float( matches.group(0) )
 
 	else:
-		raise NoDoubleError( line )
+		raise NoLatLonError( line )
 		"""Return the group if it has found a match. If not: raise our error."""
-
 
